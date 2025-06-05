752d02ad40ecf0669662005e90b29a32d3c3bfae
Andrej443 <malarovandrej349@gmail.com>
2025-06-05 22:24:29 +0300

    добавлена новая функция sumNumbers

--- src/main.java
+++ src/main.java
@@ -5,4 +5,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static int sumNumbers (int a, int b) {
+        return a + b;
+    }
 }
